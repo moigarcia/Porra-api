2f63cca1f0bbb1dbb9f17de939c801e13b4ed099
Moises Garcia <moisesgarcia83@gmail.com>
2019-10-30 11:17:51 +0100

    changes

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app = express();
 // });
 app.use(
   cors({
-    origin: process.env.URL_APP, // allow to server to accept request from different origin
+    origin: '*', // allow to server to accept request from different origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true // allow session cookie from browser to pass through
   })
